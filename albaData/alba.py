89fabd4458ea7ddc9dd3a120c96ee7e65b18646e
leejae honey <ljh23270@naver.com>
2022-06-22 02:59:22 +0900

    made brand_name_list

--- albaData/alba.py
+++ albaData/alba.py
@@ -1,9 +1,7 @@
 from unittest import result
-from extract import  extract
+from extract import extract
 import requests
 from bs4 import BeautifulSoup
-
-brand_url_list = extract()
 
 def extract_info(html):
     location = html.find("td",{"class":"local"}).text
@@ -13,11 +11,15 @@ def extract_info(html):
     reg_date = html.find("td", {"class":"regDate"}).text
     print(f'{location}/ {title}/ {time}/ {pay}/ {reg_date}')
 
-for brand_url in brand_url_list:
-    result = requests.get(brand_url)
-    soup = BeautifulSoup(result.text, "html.parser")
-    NormalInfo = soup.find("div", {"id":"NormalInfo"})
-    table = NormalInfo.find("tbody")
-    table_row_list = table.find_all("tr",{"class":""})
-    for tr in table_row_list:
-        extract_info(tr)
+def access_info(brand_url_list):
+    for brand_url in brand_url_list:
+        result = requests.get(brand_url)
+        soup = BeautifulSoup(result.text, "html.parser")
+        NormalInfo = soup.find("div", {"id":"NormalInfo"})
+        table = NormalInfo.find("tbody")
+        table_row_list = table.find_all("tr",{"class":""})
+        for tr in table_row_list:
+            extract_info(tr)
+
+brand_url_list, brand_name_list = extract()
+access_info(brand_url_list)
--- albaData/extract.py
+++ albaData/extract.py
@@ -1,5 +1,4 @@
 import os
-import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,10 +14,11 @@ def extract():
     soup = BeautifulSoup(result.text, "html.parser")
     super_brand = soup.find("div",{"id":"MainSuperBrand"})
     brand_list = super_brand.find_all("li", {"class":"impact"}) #find_all은 list로 받는게 좋음.
+    brand_name_list = super_brand.find_all("strong").text
     count = 0
     brand_url_list = []
     for brand in brand_list:
         count = count+1
         brand_url = extract_url(brand)
         brand_url_list.append(brand_url)
-    return brand_url_list
+    return brand_url_list, brand_name_list
